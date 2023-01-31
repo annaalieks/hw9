3d85388e86873b8cb3a203290eaff5291ec80c5b
annaalieks <113129285+annaalieks@users.noreply.github.com>
2023-01-31 15:41:15 +0200

    First tusk through generator function without using classes

--- script.js
+++ script.js
@@ -1,85 +1,111 @@
 'use strict';
 
 // ЗАДАЧА 1
-// ‘FizzBuzz’ через ітератор
+// ‘FizzBuzz’ через генератор
 
-class FizzBuzzIterator {
+function* fizzBuzzGenerator(start = 1, end = 100, interval = 1) {
+    let num;
 
-    constructor(start = 1, end = 100, interval = 1) {
-        this.start = start;
-        this.end = end;
-        this.interval = interval;
-    }
-
-    [Symbol.iterator]() {
-
-        let counter = 0;
-        let nextIndex = this.start;
-
-        return {
-            next: () => {
-
-                if (nextIndex <= this.end) {
-                    let result = { value: nextIndex, done: false }
-
-                    nextIndex += this.interval;
-                    counter++;
-
-                    if (counter % 3 === 0 && counter % 5 === 0) {
-                        result = { value: "FizzBuzz", done: false }
-                    } else if (counter % 5 === 0) {
-                        result = { value: "Buzz", done: false }
-                    } else if (counter % 3 == 0) {
-                        result = { value: "Fizz", done: false }
-                    }
-
-                    return result;
-                }
-                return {done: true };
-            }
+    for (num = start; num <= end; num = ++interval) {
+        
+        if (num % 15 === 0) {
+            yield num = "FizzBuzz";
+        } else if (num % 5 === 0) {
+            yield num = "Buzz";
+        } else if (num % 3 == 0) {
+            yield num = "Fizz";
+        } else {
+            yield num;
         }
     }
 }
 
-let numbersSequence = new FizzBuzzIterator;
-
-for (const num of numbersSequence) {
-    console.log(num);
-}
-
-
-// ‘FizzBuzz’ через генератор
-
-class FizzBuzzGenerator {
-
-    constructor(start = 1, end = 100, interval = 1) {
-        this.start = start;
-        this.end = end;
-        this.interval = interval;
-    }
-
-    *[Symbol.iterator]() {
-        let num;
-
-        for (num = this.start; num <= this.end; num = ++this.interval)
-        
-            if (num % 3 === 0 && num % 5 === 0) {
-                yield num = "FizzBuzz";
-            } else if (num % 5 === 0) {
-                yield num = "Buzz";
-            } else if (num % 3 == 0) {
-                yield num = "Fizz";
-            } else {
-                yield num;
-            }
-    }
-}
-
-let numSequence = new FizzBuzzGenerator;
+let numSequence = fizzBuzzGenerator();
 
 for (const num of numSequence) {
     console.log(num);
 }
+
+// ЗАДАЧА 1
+// ‘FizzBuzz’ через ітератор -  через класи
+
+// class FizzBuzzIterator {
+
+//     constructor(start = 1, end = 100, interval = 1) {
+//         this.start = start;
+//         this.end = end;
+//         this.interval = interval;
+//     }
+
+//     [Symbol.iterator]() {
+
+//         let counter = 0;
+//         let nextIndex = this.start;
+
+//         return {
+//             next: () => {
+
+//                 if (nextIndex <= this.end) {
+//                     let result = { value: nextIndex, done: false }
+
+//                     nextIndex += this.interval;
+//                     counter++;
+
+//                     if (counter % 15 === 0) {
+//                         result = { value: "FizzBuzz", done: false }
+//                     } else if (counter % 5 === 0) {
+//                         result = { value: "Buzz", done: false }
+//                     } else if (counter % 3 == 0) {
+//                         result = { value: "Fizz", done: false }
+//                     }
+
+//                     return result;
+//                 }
+//                 return {done: true };
+//             }
+//         }
+//     }
+// }
+
+// let numbersSequence = new FizzBuzzIterator;
+
+// for (const num of numbersSequence) {
+//     console.log(num);
+// }
+
+
+// // ‘FizzBuzz’ через генератор
+
+// class FizzBuzzGenerator {
+
+//     constructor(start = 1, end = 100, interval = 1) {
+//         this.start = start;
+//         this.end = end;
+//         this.interval = interval;
+//     }
+
+//     *[Symbol.iterator]() {
+//         let num;
+
+//         for (num = this.start; num <= this.end; num = ++this.interval)
+        
+//             if (num % 3 === 0 && num % 5 === 0) {
+//                 yield num = "FizzBuzz";
+//             } else if (num % 5 === 0) {
+//                 yield num = "Buzz";
+//             } else if (num % 3 == 0) {
+//                 yield num = "Fizz";
+//             } else {
+//                 yield num;
+//             }
+//     }
+// }
+
+// let numSequence = new FizzBuzzGenerator;
+
+// for (const num of numSequence) {
+//     console.log(num);
+// }
 
 
 // ЗАДАЧА 2
